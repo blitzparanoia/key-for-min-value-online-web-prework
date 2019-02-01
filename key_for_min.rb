f90db9b99ce3563bc8cf392552abe813a3d50821
Irene Saez <bluemilk44@gmail.com>
2019-02-01 00:47:36 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,7 +5,7 @@ def key_for_min_value(name_hash)
   low_key = nil
   low_key_value = nil
   hash.each do |k, v|
-    if low_key_value == nil || < low_key_value
+    if low_key_value == nil || v < low_key_value
       low_key_value = v
       low_key = k
     end
